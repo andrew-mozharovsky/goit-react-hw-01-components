8bbfa6592073f5680548bcc6aba563a50d8b11cf
AndrewMozharovsky <71605765+AndrewMozharovsky@users.noreply.github.com>
2021-05-31 14:05:52 +0300

    [home_work] add new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import user from './components/Profile/user.json';
+import statisticalData from './components/Statistics/statistical-data.json';
+import friends from './components/FriendList/friends.json';
+import transactions from './components/TransactionHistory/transactions.json';
 
 import Container from './components/Container/Container';
 import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 const App = () => {
   return (
@@ -15,6 +21,9 @@ const App = () => {
         location={user.location}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </Container>
   );
 };
